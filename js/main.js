bc5d6bb9bc34cd57ecb36f31fb4753861e730e3a
Masa <bsharehouse@gmail.com>
2016-11-12 23:47:48 -0500

    second

--- js/main.js
+++ js/main.js
@@ -23,4 +23,36 @@ jQuery(document).ready(function($){
 			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
 		});
 	}
+
+	// $('#calendar').fullCalendar({
+
+ //      header: {
+ //        left: 'prev,next today',
+ //        center: 'title',
+ //        right: 'month,listYear'
+ //      },
+
+ //      displayEventTime: false, // don't show the time column in list view
+
+ //      // THIS KEY WON'T WORK IN PRODUCTION!!!
+ //      // To make your own Google API key, follow the directions here:
+ //      // http://fullcalendar.io/docs/google_calendar/
+ //      googleCalendarApiKey: 'AIzaSyCi09m9B8YD63nc5mTg_TxHK5vyhOu13jw',
+    
+ //      // US Holidays
+ //      events: {
+ //              googleCalendarId: 'mppbf2tf0vfm4dpnqv69362i1s@group.calendar.google.com'
+ //          },
+      
+ //      eventClick: function(event) {
+ //        // opens events in a popup window
+ //        window.open(event.url, 'gcalevent', 'width=700,height=600');
+ //        return false;
+ //      },
+      
+ //      loading: function(bool) {
+ //        $('#loading').toggle(bool);
+ //      }
+      
+ //    });
 });
